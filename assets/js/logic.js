9a0ce9a7dd0d673aaa67d42ed53fd0c36910b6ba
Ginjak <89803801+Ginjak@users.noreply.github.com>
2023-12-22 10:10:01 +0000

    Add question the screen

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,3 +1,44 @@
+var gameStartDiv = document.querySelector("#start-screen");
+var gameStart = document.querySelector("#start");
+var choicesDiv = document.querySelector("#choices");
+var randomQuestionIdex;
 var questionTitle = document.querySelector("#question-title");
 
-questionTitle.textContent = "asdasd";
+gameStart.addEventListener("click", function () {
+  displayQuestion();
+  hideStartGame();
+});
+
+function hideStartGame() {
+  gameStartDiv.setAttribute("class", "hide");
+}
+
+function randomQuestion(arr) {
+  randomQuestionIdex = Math.floor(Math.random() * arr.length);
+  console.log(randomQuestionIdex);
+}
+
+function handleButtonClick(event) {
+  var key = event.target.key;
+  console.log("Key for the clicked button: " + key);
+
+  if (key === "correct") {
+    console.log("Correct answer!");
+  } else {
+    console.log("Incorrect answer.");
+  }
+}
+
+function displayQuestion() {
+  randomQuestion(questionsArr);
+  questionTitle.textContent = questionsArr[randomQuestionIdex].question;
+  for (var answer in questionsArr[randomQuestionIdex]) {
+    var choiceButton = document.createElement("button");
+    choiceButton.textContent = questionsArr[randomQuestionIdex][answer];
+    choiceButton.key = answer;
+    choicesDiv.appendChild(choiceButton);
+    choiceButton.addEventListener("click", handleButtonClick);
+  }
+}
+
+randomQuestion(questionsArr);
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -3,11 +3,13 @@ var questionsArr = [
     question: "question one",
     answerOne: "Answer 1",
     andwerTwo: "Answer 2",
+    correct: "Correct answer",
   },
   {
-    question: "question one",
-    answerOne: "Answer 1",
-    andwerTwo: "Answer 2",
+    question: "question Two",
+    answerOne: "Answer Two 1",
+    andwerTwo: "Answer Two 2",
+    correct: "Correct answer",
   },
 ];
 console.log(questionsArr[0].question);
